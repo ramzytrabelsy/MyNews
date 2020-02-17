195c0610f6558c75474705f43f3de45aacacbf2c
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2020-02-17 23:16:16 +0100

    feat: add action to navigate & integration AboutScreen

--- src/Session/AboutView.js
+++ src/Session/AboutView.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View } from 'react-native';
 import {
-  Container, Header, Left, Body, Title, Right, Content, Text, Icon, Button,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Image,
+  Alert,
+  ScrollView,
+  FlatList,
+  Button,
+} from 'react-native'
+import {
+  Container, Header, Left, Body, Title, Right, Content, Text, Icon,
 } from 'native-base';
 
 import * as PropTypes from '../common/proptypes';
@@ -33,18 +42,118 @@ class AboutView extends Component {
           <Left>
           </Left>
           <Body>
-    <Title>Details</Title>
+            <Title>Details</Title>
           </Body>
           <Right />
         </Header>
 
         <Content padder>
-    <Text>{item.title}</Text>
+          <View style={styles.container}>
+            <ScrollView>
+              <View style={{ alignItems: 'center', marginHorizontal: 30 }}>
+                <Image style={styles.productImg} source={{ uri: item.urlToImage }} />
+                <Text style={styles.name}>{item.title}</Text>
+                <Text style={styles.price}>{item.author}</Text>
+                <Text style={styles.description}>{item.description}</Text>
+              </View>
+
+            </ScrollView>
+          </View>
         </Content>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+  },
+  productImg: {
+    width: 200,
+    height: 200,
+  },
+  name: {
+    fontSize: 28,
+    color: "#696969",
+    fontWeight: 'bold'
+  },
+  price: {
+    marginTop: 10,
+    fontSize: 18,
+    color: "green",
+    fontWeight: 'bold'
+  },
+  description: {
+    textAlign: 'center',
+    marginTop: 10,
+    color: "#696969",
+  },
+  star: {
+    width: 40,
+    height: 40,
+  },
+  btnColor: {
+    height: 30,
+    width: 30,
+    borderRadius: 30,
+    marginHorizontal: 3
+  },
+  btnSize: {
+    height: 40,
+    width: 40,
+    borderRadius: 40,
+    borderColor: '#778899',
+    borderWidth: 1,
+    marginHorizontal: 3,
+    backgroundColor: 'white',
+
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  starContainer: {
+    justifyContent: 'center',
+    marginHorizontal: 30,
+    flexDirection: 'row',
+    marginTop: 20
+  },
+  contentColors: {
+    justifyContent: 'center',
+    marginHorizontal: 30,
+    flexDirection: 'row',
+    marginTop: 20
+  },
+  contentSize: {
+    justifyContent: 'center',
+    marginHorizontal: 30,
+    flexDirection: 'row',
+    marginTop: 20
+  },
+  separator: {
+    height: 2,
+    backgroundColor: "#eeeeee",
+    marginTop: 20,
+    marginHorizontal: 30
+  },
+  shareButton: {
+    marginTop: 10,
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    backgroundColor: "#00BFFF",
+  },
+  shareButtonText: {
+    color: "#FFFFFF",
+    fontSize: 20,
+  },
+  addToCarContainer: {
+    marginHorizontal: 30
+  }
+});
 
 AboutView.propTypes = propTypes;
 
